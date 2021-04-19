7bfbcbade130caf9b891667c267f92ca63627de1
PolitePp <vladimirk96rus@gmail.com>
2021-04-19 12:35:55 +0500

    Fix run.sh script

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker network create verticaspark
 cd second_task/input_hdfs
 unzip transactions.zip
 cd ../../
+docker-compose up -d
 docker exec -it pg_db pg_dump -h localhost -U postgres -F c -f /opt/input_data/dump.tar.gz postgres
 docker exec -it namenode hdfs dfs -mkdir /raw
 docker exec -it namenode hdfs dfs -put /opt/input_hdfs/transactions.json /raw
